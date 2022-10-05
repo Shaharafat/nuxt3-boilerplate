9303fedd3e77eeb68a33a2165fec20f58b577f85
shah_arafat <shah@co.design>
2022-10-05 21:57:03 +0600

    initial setup and scss setup done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
 	vite: {
 		css: {
 			preprocessorOptions: {
+				// This SCSS option below will make available SCSS codes globally.
 				scss: {
 					additionalData:
 						'@use "@/assets/scss/_variables.scss" as *; @use "@/assets/scss/main.scss" as *; @use "@/assets/scss/_mixins.scss" as *;',
