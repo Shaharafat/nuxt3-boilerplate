8795c84e7d427084f8950b7b05f28433df8a36e7
shah_arafat <shah@co.design>
2022-10-17 17:48:18 +0600

    updated

--- plugins/settings.server.ts
+++ plugins/settings.server.ts
@@ -1,9 +1,10 @@
 import { defineNuxtPlugin } from '#app'
+import { useSettingsStore } from '~~/stores/settings'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   if (process.server) {
-    // const settings = useSettingsStore()
-    // await settings.loadSettings()
-    // console.log(settings.getAllSettings)
+    const settings = useSettingsStore()
+    await settings.loadSettings()
+    console.log(settings.getAllSettings)
   }
 })
